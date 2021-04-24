3c9f001e66ca4400a03063449ef96b608246d2bf
Quhu <hung.vq98@gmail.com>
2021-04-24 09:59:51 +0700

    AdminPage + Product Manager

---
+++ ChocoGear/ChocoGear/Areas/Admin/Controllers/AdminSiteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChocoGear.Areas.Admin.Controllers
+{
+    public class AdminSiteController : Controller
+    {
+        // GET: Admin/AdminSite
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
